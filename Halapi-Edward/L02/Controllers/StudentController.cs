0d07651d837aed72361347496a0efb30ab6a8b90
Edward <edwardhalapi@hsoftdev.org>
2021-10-13 20:14:17 +0300

    started CRUD operations

--- Halapi-Edward/L02/Controllers/StudentController.cs
+++ Halapi-Edward/L02/Controllers/StudentController.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -23,6 +20,11 @@ namespace Lab02.Controllers
         {
             StudentRepo stud = new StudentRepo();
             return stud.student[0];
+        }
+        public Student Delete()
+        {
+            StudentRepo stud = new StudentRepo();
+            return null;
         }
     }
 }
