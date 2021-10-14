9f8c47b73bcf2ae08a3e87e492b8ec4749b80a53
Edward <edwardhalapi@hsoftdev.org>
2021-10-14 20:56:48 +0300

    prepared CRUD operations

--- Halapi-Edward/L02/Controllers/StudentController.cs
+++ Halapi-Edward/L02/Controllers/StudentController.cs
@@ -21,9 +21,20 @@ namespace Lab02.Controllers
             StudentRepo stud = new StudentRepo();
             return stud.student[0];
         }
+        [HttpDelete]
         public Student Delete()
         {
             StudentRepo stud = new StudentRepo();
+            return null;
+        }
+        [HttpPut]
+        public Student Put()
+        {
+            return null;           
+        }
+        [HttpPost]
+        public Student Post()
+        {
             return null;
         }
     }
